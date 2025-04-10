562eeb82946d36ab7fbd116955cda082bd43ba22
joshlevine55 <46460705+joshlevine55@users.noreply.github.com>
2025-04-10 16:13:54 -0400

    Update gcp.sh

--- Chapter08/sample1/gcp.sh
+++ Chapter08/sample1/gcp.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-apt update && apt install -y curl apt-transport-https ca-certificates gnupg 
+set -e  # optional, but recommended to exit on error
 
-echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" \ 
+apt update && apt install -y curl apt-transport-https ca-certificates gnupg
 
-  | tee /etc/apt/sources.list.d/google-cloud-sdk.list 
+# Add GCP Cloud SDK repo
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" \
+  | tee /etc/apt/sources.list.d/google-cloud-sdk.list
 
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg \ 
+# Download and add Google Cloud public key
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg \
+  | apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
 
-  | apt-key --keyring /usr/share/keyrings/cloud.google.gpg add - 
+# Update and install
+apt update && apt install -y google-cloud-sdk
 
-apt update && apt install -y google-cloud-sdk 
