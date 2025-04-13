c1eef53a5b37af75efd0e6ca558008c72f484708
joshlevine55 <46460705+joshlevine55@users.noreply.github.com>
2025-04-13 14:02:38 -0400

    Update acceptance-test.sh

--- Chapter08/sample1/acceptance-test.sh
+++ Chapter08/sample1/acceptance-test.sh
@@ -3,4 +3,4 @@ set -x
 
 NODE_IP=$(kubectl --insecure-skip-tls-verify get nodes -o jsonpath='{ $.items[0].status.addresses[?(@.type=="InternalIP")].address }')
 NODE_PORT=$(kubectl --insecure-skip-tls-verify get svc calculator-service -o=jsonpath='{.spec.ports[0].nodePort}')
-./gradlew acceptanceTest -Dcalculator.url=http://${NODE_IP}:${NODE_PORT}
+./gradlew acceptanceTest -Dcalculator.url=http://172.18.0.4:${NODE_PORT}
